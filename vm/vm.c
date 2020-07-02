6e006b7ab4cfdf66b2abf96f406bddabe3f2d913
dfbsdan <dfbsdaniel11@hotmail.com>
2020-07-02 22:37:29 +0900

    Testing

--- vm/vm.c
+++ vm/vm.c
@@ -243,14 +243,15 @@ vm_handle_wp (struct page *page UNUSED) {
 /* Checks if the given page with address PGADDR is the one on top of a stack
  * page. */
 static bool
-stack_pg (void *pgaddr, void *esp, struct supplemental_page_table *spt) {
+is_stack_pg (void *pgaddr, void *esp, struct supplemental_page_table *spt) {
 	struct page *page;
+	esp = pg_round_down (esp);
 
 	ASSERT (vm_is_page_addr (pgaddr) && is_user_vaddr (pgaddr));
 	ASSERT (esp && is_user_vaddr (esp));
 	ASSERT (spt);
 
-	return pgaddr == pg_round_down (esp)
+	return pgaddr == esp
 			|| ((page = spt_find_page (spt, pgaddr - 1))
 					&& page->operations->type == VM_ANON
 					&& page->anon.a_type == ANON_STACK);
@@ -273,12 +274,8 @@ vm_try_handle_fault (struct intr_frame *f UNUSED, void *addr, bool user,
 			if (!page) { //Unexisting page
 				/* Recover if stack overflow. */
 				////////////////////////////////////////////////////////////////////////TESTING
-				printf("vm_try_handle_fault: pgaddr: %p, esp: %p\n", pg_va, (void*)f->rsp);
-				if (addr >= (void*)f->rsp - 8 && stack_pg (pg_va, (void*)f->rsp, spt)) {
-					printf("vm_try_handle_fault: Growing stack, esp is page: %d\n", NULL != spt_find_page (spt, (void*)f->rsp));
-					//printf("vm_try_handle_fault: pgaddr: %p, page below is stack: %d\n", pg_va);
+				if (addr >= (void*)f->rsp - 8 && is_stack_pg (pg_va, (void*)f->rsp, spt))
 					return vm_stack_growth (addr);
-				}
 				///////////////////////////////////////////////////////////////////////////////
 				return false; //Unexisting non-stack page
 			}
